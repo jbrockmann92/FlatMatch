0e719db695c6f26cad32680c1ca190ffb3624a8f
Abraham Sanchez <sanchez95san@gmail.com>
2020-03-05 16:26:30 -0600

    Models have been partially completed. Property info model still needs to be included

---
+++ FlatMatchApp/FlatMatchApp/Models/Leaseholder.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlatMatchApp.Models
+{
+    public class Leaseholder
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [ForeignKey("Property")]
+        public int PropertyId { get; set; }
+        public Property Address { get; set; }
+        [ForeignKey("IdentityUser")]
+        [Display(Name = "Identity User")]
+        public string UserId { get; set; }
+        public IdentityUser IdentityUser { get; set; }
+    }
+}
---
+++ FlatMatchApp/FlatMatchApp/Models/Property.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlatMatchApp.Models
+{
+    public class Property
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string StreetName { get; set; }
+        [Required]
+        public string ApartmentNumber { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string State { get; set; }
+        [Required]
+        [DataType(DataType.PostalCode)]
+        public string ZipCode { get; set; }
+    }
+}
