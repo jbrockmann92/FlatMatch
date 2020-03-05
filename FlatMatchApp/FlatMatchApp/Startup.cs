c0be053f8cac01328d683a81c79897eb88d761d7
Abraham Sanchez <sanchez95san@gmail.com>
2020-03-05 16:39:34 -0600

    Correction made in startup code, requireConfirmAccount has now been set to false

--- FlatMatchApp/FlatMatchApp/Startup.cs
+++ FlatMatchApp/FlatMatchApp/Startup.cs
@@ -31,7 +31,7 @@ namespace FlatMatchApp
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
 
-            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultUI()
                 .AddDefaultTokenProviders();
