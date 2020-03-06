9648fcd3f42a984e8ab46949ed875d51c1834114
David steinhafel <davidsteinhafel@outlook.com>
2020-03-06 10:43:16 -0600

    added create get and post methods and details get

--- FlatMatchApp/FlatMatchApp/Controllers/LeaseHolderController.cs
+++ FlatMatchApp/FlatMatchApp/Controllers/LeaseHolderController.cs
@@ -1,70 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using FlatMatchApp.Data;
+using FlatMatchApp.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace FlatMatchApp.Controllers
 {
-    public class LeaseHolderController : Controller
+    public class LeaseholderController : Controller
     {
+        private ApplicationDbContext _context;
+        public LeaseholderController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         // GET: LeaseHolder
+
         public ActionResult Index()
         {
             return View();
         }
 
         // GET: LeaseHolder/Details/5
-        public ActionResult Details(int id)
+        [HttpGet]
+        public IActionResult Details(int id)
         {
-            return View();
+            var leaseholder = _context.Leaseholders.SingleOrDefault(x => x.Id == id);
+            return View(leaseholder);
         }
 
         // GET: LeaseHolder/Create
-        public ActionResult Create()
+        [HttpGet]
+        public IActionResult Create()
         {
-            return View();
+
+            var leaseholder = new Leaseholder();
+            return View(leaseholder);
+
+
+
         }
 
         // POST: LeaseHolder/Create
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public IActionResult Create(Leaseholder leaseholder)
         {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+            var userId = this.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            leaseholder.UserId = userId;
+            _context.Leaseholders.Add(leaseholder);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
 
         // GET: LeaseHolder/Edit/5
         public ActionResult Edit(int id)
         {
-            return View();
+            
         }
 
         // POST: LeaseHolder/Edit/5
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public IActionResult Edit(Leaseholder leaseholder)
         {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
+           
         }
 
         // GET: LeaseHolder/Delete/5
