c75a2584150880b1f4c5b8389e324fa75109182a
Jacob Brockmann <jacob.brockmann92@gmail.com>
2020-03-06 06:44:20 -0600

    Added preference class. Shouldn 't need classes for all preferences, as we can just do instances of the class for each of the 10-20 preferences we'll have.

---
+++ FlatMatchApp/FlatMatchApp/Models/Preference.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlatMatchApp.Models
+{
+    public class Preference
+    {
+        public bool Exists { get; set; }
+        public bool Value { get; set; }
+    }
+}
