c8c34cb9e790dbf68bedc5b56b76d75a61f7f92f
Jacob Martinez <jmartinez.data@gmail.com>
2020-03-09 09:28:10 -0500

    Added Migration for updated models

--- FlatMatchApp/FlatMatchApp/Data/Migrations/20200309142239_updatedAddressAndRenterViewModel.Designer.cs
+++ FlatMatchApp/FlatMatchApp/Data/Migrations/20200309142239_updatedAddressAndRenterViewModel.Designer.cs
@@ -4,14 +4,16 @@ using FlatMatchApp.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FlatMatchApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200309142239_updatedAddressAndRenterViewModel")]
+    partial class updatedAddressAndRenterViewModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
